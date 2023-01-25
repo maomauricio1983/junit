d3283117fad684869a7443a40afd5cfc5497f732
Arnold Mauricio <maomauriciomunoz@gmail.com>
2023-01-25 16:49:14 -0500

    implementamos los metodos debito() y credito() en la clase Cuenta y implementamos los metodos de prueba testDebitoCuenta() y testCreditoCuenta()

--- src/main/java/org/amunoz/app/models/Cuenta.java
+++ src/main/java/org/amunoz/app/models/Cuenta.java
@@ -10,7 +10,17 @@ public class Cuenta {
 
     public Cuenta(String persona, BigDecimal saldo) {
         this.saldo = saldo;
-        this.persona= persona;
+        this.persona = persona;
+    }
+
+    // metodo que le resta a saldo
+    public void debito(BigDecimal monto) {
+        this.saldo = this.saldo.subtract(monto);
+    }
+
+    // metodo que le suma a saldo
+    public void credito(BigDecimal monto) {
+        this.saldo = this.saldo.add(monto);
     }
 
     public String getPersona() {
--- src/test/java/org/amunoz/app/models/CuentaTest.java
+++ src/test/java/org/amunoz/app/models/CuentaTest.java
@@ -16,6 +16,7 @@ class CuentaTest {
 //        cuenta.setPersona("Mauricio");
         String esperado = "Mauricio";
         String real = cuenta.getPersona();
+        assertNotNull(real); //verifica que real NO sea null
         assertEquals(esperado, real);
         assertTrue(real.equals("Mauricio"));
     }
@@ -23,6 +24,7 @@ class CuentaTest {
     @Test
     void testSaldoCuenta() {
         Cuenta cuenta = new Cuenta("Andres", new BigDecimal("1000.12345"));
+        assertNotNull(cuenta.getSaldo()); //verifica que el saldo NO sea null
         assertEquals(1000.12345, cuenta.getSaldo().doubleValue());
         assertFalse(cuenta.getSaldo().compareTo(BigDecimal.ZERO) < 0);  //prueba si el saldo es mayor de cero, "el resultado debe ser falso"
     }
@@ -35,4 +37,24 @@ class CuentaTest {
 //        assertNotEquals(cuenta2,cuenta);  // valida que NO sean objetos iguales
         assertEquals(cuenta2, cuenta); // valida que sean objetos iguales
     }
+
+    @Test
+    void testDebitoCuenta() {
+        Cuenta cuenta = new Cuenta("Andres", new BigDecimal("1000.12345")); // asignamos los valores al objeto cuenta
+        cuenta.debito(new BigDecimal(100)); // implementamos el metodo debito y le pasamos como parametro un 100
+        assertNotNull(cuenta.getSaldo()); // verifica que el saldo no sea null
+        assertEquals(900, cuenta.getSaldo().intValue()); // compara el valor esperado con el real pero conviertiendo el real a un entero
+        assertEquals("900.12345", cuenta.getSaldo().toPlainString()); // compara el valor esperado con el real
+    }
+
+    @Test
+    void testCreditoCuenta() {
+        Cuenta cuenta = new Cuenta("Andres", new BigDecimal("1000.12345")); // asignamos los valores al objeto cuenta
+        cuenta.credito(new BigDecimal(100)); // implementamos el metodo credito y le pasamos como parametro un 100
+        assertNotNull(cuenta.getSaldo()); // verifica que el saldo no sea null
+        assertEquals(1100, cuenta.getSaldo().intValue()); // compara el valor esperado con el real pero conviertiendo el real a un entero
+        assertEquals("1100.12345", cuenta.getSaldo().toPlainString()); // compara el valor esperado con el real
+    }
+
+
 }
